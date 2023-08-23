5258f328976d0a5a25b8b77df0e9aa7c5b45d2b3
Allan Oliveira <allanolme@gmail.com>
2023-08-23 17:50:20 -0300

    create card

---
+++ listApp/src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export function Card(){
+	return(
+		<div>
+			<strong>Rodrigo Gonçalvez</strong>
+			<small>10:15:24</small>
+		</div>
+	)
+}
--- listApp/src/pages/home/index.jsx
+++ listApp/src/pages/home/index.jsx
@@ -1,9 +1,18 @@
 import './style.css';
 
+import { Card } from '../../components/Card'
+
 export function Home(){
 	return(
 		<div>
-			<p>teste</p>
+			<h1>Lista de Presença</h1>
+			<input type="text" placeholder="Digite o nome..."/>
+			<button>Adicionar</button>
+			
+			<Card/>
+			<Card/>
+			<Card/>
+			<Card/>
 		</div>
 	)
 }
