7a125c8e516f00af6977aeaf7a03942c1d578fd8
Allan Oliveira <allanolme@gmail.com>
2023-08-23 18:14:40 -0300

    Add State

--- listApp/src/pages/home/index.jsx
+++ listApp/src/pages/home/index.jsx
@@ -1,13 +1,16 @@
+import React, {useState} from 'react';
 import './style.css';
 
 import { Card } from '../../components/Card'
 
 export function Home(){
+	const [studentName, setStudentName] = useState();
+	
 	return(
 		<div>
 			<h1>Lista de Presença</h1>
-			<input type="text" placeholder="Digite o nome..."/>
-			<button>Adicionar</button>
+			<input type="text" placeholder="Digite o nome..." onChange={e => setStudentName(e.target.value)}/>
+			<button>Adicionar {studentName}</button>
 			
 			<Card name="primeiro" city="1"/>
 			<Card name="segundo" city="2"/>
